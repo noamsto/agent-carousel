cfb714f3505087e780800a11c77fd5749eff5298
Noam Stolero <noam@factify.com>
2026-06-14 11:46:36 +0300

    build: add d2 + resvg to devShell; document diagram deps

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
           inherit (config.pre-commit) shellHook;
           packages =
             config.pre-commit.settings.enabledPackages
-            ++ [pkgs.go pkgs.gopls pkgs.gotools pkgs.golangci-lint pkgs.chafa pkgs.bats pkgs.goreleaser pkgs.gh];
+            ++ [pkgs.go pkgs.gopls pkgs.gotools pkgs.golangci-lint pkgs.chafa pkgs.bats pkgs.goreleaser pkgs.gh pkgs.d2 pkgs.resvg];
         };
 
         packages = {
